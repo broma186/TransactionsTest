41a8ca4cb77c52ae5b7c6e25e041ca8b1ef2f92d
Matthew Brooker <broma186@github.com>
2024-09-10 20:58:32 +1200

    Filtering functionality

--- app/src/main/java/com/example/kbandroidtechassessment/screen/TravelSavingsScreen.kt
+++ app/src/main/java/com/example/kbandroidtechassessment/screen/TravelSavingsScreen.kt
@@ -2,12 +2,15 @@ package com.example.kbandroidtechassessment.screen
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -16,6 +19,7 @@ import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,6 +27,7 @@ import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.kbandroidtechassessment.components.TransactionDateRangeFilter
 import com.example.kbandroidtechassessment.components.TransactionItem
 import com.example.kbandroidtechassessment.model.Transaction
 import com.example.kbandroidtechassessment.ui.theme.KBAndroidTechAssessmentTheme
@@ -32,14 +37,32 @@ import com.example.kbandroidtechassessment.viewmodel.TravelSavingsViewModel
 fun TravelSavingsScreen() {
     val viewModel: TravelSavingsViewModel = viewModel()
     KBAndroidTechAssessmentTheme {
-        TravelSavingsScreenContent(viewModel.transactions.value)
+        TravelSavingsScreenContent(
+            viewModel.transactions.value,
+            viewModel.availableBalance.value,
+            viewModel.showDatePicker.value,
+            viewModel.fromDateStr.value,
+            viewModel.toDateStr.value,
+            viewModel::toggleDatePicker,
+            viewModel::updateFromDate,
+            viewModel::updateToDate,
+            viewModel::resetDateRange
+        )
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TravelSavingsScreenContent(
-    transactions: List<Transaction>
+    transactions: List<Transaction>,
+    availableBalance: String,
+    showDatePicker: Boolean,
+    fromDateStr: String,
+    toDateStr: String,
+    toggleDatePicker: () -> Unit,
+    updateFromDate: (updatedDate: Long) -> Unit,
+    updateToDate: (updatedDate: Long) -> Unit,
+    resetDateRange: () -> Unit
 ) {
     Scaffold(modifier = Modifier.fillMaxSize(),
         topBar = {
@@ -48,17 +71,32 @@ fun TravelSavingsScreenContent(
                     Text("Travel Savings Account")
                 },
                 actions = {
-                    IconButton(onClick = { /* Handle filter action */ }) {
+                    IconButton(onClick = {
+                        toggleDatePicker()
+                    }) {
                         Icon(
                             imageVector = Icons.Default.DateRange,
-                            contentDescription = "Filter"
+                            contentDescription = "Filter Transactions"
                         )
                     }
                 }
             )
         }
     ) { innerPadding ->
+        if (showDatePicker) {
+            TransactionDateRangeFilter(onDateRangeSelected = {
+                it.first?.let { fromDate ->
+                    updateFromDate(fromDate)
+                }
+                it.second?.let { toDate ->
+                    updateToDate(toDate)
+                }
+            }) {
+                toggleDatePicker()
+            }
+        }
         Column(modifier = Modifier.padding(innerPadding)) {
+
             Row(modifier = Modifier.padding(16.dp)) {
                 Text(
                     text = "Available Balance:  ",
@@ -66,10 +104,41 @@ fun TravelSavingsScreenContent(
                     fontSize = TextUnit(16f, TextUnitType.Sp),
                 )
                 Text(
-                    text = "$0.00",
+                    text = availableBalance,
                     fontWeight = FontWeight.W600,
                     fontSize = TextUnit(24f, TextUnitType.Sp),
                 )
+            }
+            Row(modifier = Modifier.padding(16.dp),
+                verticalAlignment = Alignment.CenterVertically) {
+                Column {
+                    Row {
+                        Text(
+                            text = "From Date: $fromDateStr",
+                            fontWeight = FontWeight.W300,
+                            fontSize = TextUnit(16f, TextUnitType.Sp)
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Row {
+                        Text(
+                            text = "To Date: $toDateStr",
+                            fontWeight = FontWeight.W300,
+                            fontSize = TextUnit(16f, TextUnitType.Sp)
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                }
+                Modifier.padding(32.dp)
+                Button({
+                    resetDateRange()
+                }) {
+                    Text(
+                        text = "RESET",
+                        fontWeight = FontWeight.W600,
+                        fontSize = TextUnit(16f, TextUnitType.Sp)
+                    )
+                }
             }
             HorizontalDivider()
 
@@ -86,6 +155,15 @@ fun TravelSavingsScreenContent(
 @Composable
 private fun TravelSavingsScreenPreview() {
     KBAndroidTechAssessmentTheme {
-        TravelSavingsScreenContent((viewModel() as TravelSavingsViewModel).transactions.value)
+        TravelSavingsScreenContent(
+            (viewModel() as TravelSavingsViewModel).transactions.value,
+            "$0.00",
+            false,
+            "01/09/2024",
+            "31/09/2024",
+            { 3 },
+            { 4 },
+            {}
+        ) {}
     }
 }
--- app/src/main/java/com/example/kbandroidtechassessment/viewmodel/TravelSavingsViewModel.kt
+++ app/src/main/java/com/example/kbandroidtechassessment/viewmodel/TravelSavingsViewModel.kt
@@ -6,12 +6,19 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.kbandroidtechassessment.model.Transaction
 import dagger.hilt.android.lifecycle.HiltViewModel
+import java.time.Instant
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.Currency
+import java.util.Locale
 import javax.inject.Inject
 
 @HiltViewModel
 class TravelSavingsViewModel @Inject constructor(): ViewModel() {
 
-    private val _transactions: MutableState<List<Transaction>> = mutableStateOf(listOf(
+    private val transactionList = listOf(
         Transaction("2024-09-22", "Restaurant", -35.00),
         Transaction("2024-09-24", "Car Repair", -150.00),
         Transaction("2024-09-11", "Utilities", -150.00),
@@ -29,7 +36,7 @@ class TravelSavingsViewModel @Inject constructor(): ViewModel() {
         Transaction("2024-09-30", "Gym Membership", -50.00),
         Transaction("2024-09-15", "Vacation", -1500.00),
         Transaction("2024-09-07", "Movie Tickets", -30.00),
-        Transaction("2024-09-08", "Salary", 2500.00),
+        Transaction("2024-09-08", "Salary", 6500.00),
         Transaction("2024-09-09", "Groceries", -80.00),
         Transaction("2024-09-23", "Groceries", -90.00),
         Transaction("2024-09-10", "Rent", -1200.00),
@@ -42,11 +49,77 @@ class TravelSavingsViewModel @Inject constructor(): ViewModel() {
         Transaction("2024-09-16", "Restaurant", -40.00),
         Transaction("2024-09-03", "Restaurant", -35.00),
         Transaction("2024-09-04", "Gas Station", -60.00),
-    ))
+    )
+
+    private val _fromDate: MutableState<LocalDate?> = mutableStateOf(null)
+
+    private val _toDate: MutableState<LocalDate?> = mutableStateOf(null)
+
+    private val _fromDateStr: MutableState<String> = mutableStateOf(formatStringDate(_fromDate.value))
+    val fromDateStr: MutableState<String> = _fromDateStr
+
+    private val _toDateStr: MutableState<String> = mutableStateOf(formatStringDate(_toDate.value))
+    val toDateStr: MutableState<String> = _toDateStr
+
+    private val _transactions: MutableState<List<Transaction>> = mutableStateOf(calculateRelevantTransactions(transactionList))
     val transactions: State<List<Transaction>> = _transactions
 
+    private val _availableBalance: MutableState<String> = mutableStateOf(calculateCurrentBalance())
+    val availableBalance: State<String> = _availableBalance
 
+    private val _showDatePicker: MutableState<Boolean> = mutableStateOf(false)
+    val showDatePicker: State<Boolean> = _showDatePicker
 
+    private val dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+
+    private fun calculateRelevantTransactions(transactions: List<Transaction>): List<Transaction> {
+        return if (_fromDate.value != null && _toDate.value != null) {
+            transactions.filter {
+                val transactionDate = LocalDate.parse(it.date, dateFormat)
+                transactionDate >= _fromDate.value
+                        && transactionDate <= _toDate.value
+            }
+        } else {
+            transactions
+        }
+    }
+
+    private fun calculateCurrentBalance(): String {
+        return "${Currency.getInstance(Locale.getDefault()).symbol}${_transactions.value.sumOf { it.amount }.toFloat()}"
+    }
+
+    private fun formatStringDate(dateToFormat: LocalDate?): String {
+        return dateToFormat?.let {
+            dateFormat.format(dateToFormat)
+        } ?: "Add Date"
+    }
+
+    fun toggleDatePicker() {
+        _showDatePicker.value = !_showDatePicker.value
+    }
+
+    fun updateFromDate(updatedDate: Long) {
+        _fromDate.value = LocalDateTime.ofInstant(Instant.ofEpochMilli(updatedDate), ZoneId.systemDefault()).toLocalDate()
+        _fromDateStr.value = formatStringDate(_fromDate.value)
+        _transactions.value = calculateRelevantTransactions(transactionList)
+        _availableBalance.value = calculateCurrentBalance()
+    }
+
+    fun updateToDate(updatedDate: Long) {
+        _toDate.value = LocalDateTime.ofInstant(Instant.ofEpochMilli(updatedDate), ZoneId.systemDefault()).toLocalDate()
+        _toDateStr.value = formatStringDate(_toDate.value)
+        _transactions.value = calculateRelevantTransactions(transactionList)
+        _availableBalance.value = calculateCurrentBalance()
+    }
+
+    fun resetDateRange() {
+        _fromDate.value = null
+        _toDate.value = null
+        _fromDateStr.value = formatStringDate(null)
+        _toDateStr.value = formatStringDate(null)
+        _transactions.value = calculateRelevantTransactions(transactionList)
+        _availableBalance.value = calculateCurrentBalance()
+    }
 }
 
 
